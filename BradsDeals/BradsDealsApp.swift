bb4da0a7212357cb4e91853aa549f3be0b0914d5
David Williams <davidlwilliams423@gmail.com>
2024-01-12 17:02:42 -0500

    Initial Commit

---
+++ BradsDeals/BradsDealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BradsDealsApp.swift
+//  BradsDeals
+//
+//  Created by David Williams on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BradsDealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
